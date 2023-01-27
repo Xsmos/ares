52707d3853e98bb044c50726d7fda8d6963b1df1
Xsmos <xiabin.phy@gmail.com>
2023-01-27 18:27:17 -0500

    atol and rtol matter, but too high accuracy leads to errors. conclusion: bug from rounding

--- ares/static/ChemicalNetwork.py
+++ ares/static/ChemicalNetwork.py
@@ -394,7 +394,7 @@ class ChemicalNetwork(object):
             raise ValueError(err)
 
         # t2time = lambda x, y : x/y if y != 0 else '1/0'
-        print('z =', z, 'xe =', q[1], 'dxedt =', self.dqdt[1], 't =', t)#, 'time =', time, 't/time =', t2time(t,time)) # added by Bin Xia
+        # print('z =', z, 'xe =', q[1], 'dxedt =', self.dqdt[1], 't =', t, 'time =', time)#, 't/time =', t2time(t,time)) # added by Bin Xia
         # with open('xe_data.txt', 'a') as f:# Xia
         #     f.write(str(q[1]) + '\n')# Xia
 
