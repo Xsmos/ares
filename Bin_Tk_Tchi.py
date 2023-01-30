28e28eb02451cfa25b3de242c93b80e3c985517e
Xsmos <xiabin.phy@gmail.com>
2023-01-29 22:06:05 -0500

    dark matter temperature is too low for v_stream=27km/s case

--- Bin_Tk_Tchi.py
+++ Bin_Tk_Tchi.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 sim = ares.simulations.Global21cm(radiative_transfer=False, verbose=False, dark_matter_heating=False)
 sim.run()
-plt.plot(sim.history['z'], sim.history['igm_Tk'],
+plt.plot(1+sim.history['z'], sim.history['igm_Tk'],
          label='igm_Tk_default', linestyle='-', c='k')
 history_z = sim.history['z']
 # history_cgm_e = sim.history['cgm_e']
@@ -12,42 +12,42 @@ history_igm_e = sim.history['igm_e']
 
 
 sim = ares.simulations.Global21cm(
-    verbose=False, dark_matter_heating=True, include_cgm=False, initial_v_stream = 0, initial_redshift=1010)#300)#
+    radiative_transfer=False, verbose=False, dark_matter_heating=True, include_cgm=False, initial_v_stream = 1, initial_redshift=1010)#300)#
 sim.run()
-plt.plot(sim.history['z'], 2.73*(1+sim.history['z']),
+plt.plot(1+sim.history['z'], 2.73*(1+sim.history['z']),
          label='Tgamma', c='green', linestyle=':')
-plt.plot(sim.history['z'], sim.history['igm_Tk'],
-         label=r'igm_Tk, $V_{\chi b,0}=0$', linestyle='--', c='b')
+plt.plot(1+sim.history['z'], sim.history['igm_Tk'],
+         label=r'igm_Tk, $V{\chi b,0}=$'+'{}m/s'.format(sim.pf['initial_v_stream']), linestyle='--', c='b')
 if sim.pf["dark_matter_heating"]:
-    plt.plot(sim.history['z'], sim.history['igm_Tchi'],
-             label=r'igm_Tchi, $V_{\chi b,0}=0$', c='b', linestyle='-.')
+    plt.plot(1+sim.history['z'], sim.history['igm_Tchi'],
+             label=r'igm_Tchi, $V{\chi b,0}=$'+'{}m/s'.format(sim.pf['initial_v_stream']), c='b', linestyle='-.')
 
 
 sim = ares.simulations.Global21cm(
-    verbose=False, dark_matter_heating=True, include_cgm=False, initial_redshift=1010)#300)#
+    radiative_transfer=False, verbose=False, dark_matter_heating=True, include_cgm=False, initial_v_stream = 29000, initial_redshift=1010)#300)#
 sim.run()
-# plt.plot(sim.history['z'], 2.73*(1+sim.history['z']),
+# plt.plot(1+sim.history['z'], 2.73*(1+sim.history['z']),
 #          label='Tgamma', c='green', linestyle=':')
-plt.plot(sim.history['z'], sim.history['igm_Tk'],
-         label='igm_Tk, $V_{\chi b,0}=29$ km/s', linestyle='--', c='r')
+plt.plot(1+sim.history['z'], sim.history['igm_Tk'],
+         label='igm_Tk, $V{\chi b,0}=$'+'{}m/s'.format(sim.pf['initial_v_stream']), linestyle='--', c='r')
 if sim.pf["dark_matter_heating"]:
-    plt.plot(sim.history['z'], sim.history['igm_Tchi'],
-             label='igm_Tchi, $V_{\chi b,0}=29$ km/s', c='r', linestyle='-.')
+    plt.plot(1+sim.history['z'], sim.history['igm_Tchi'],
+             label='igm_Tchi, $V{\chi b,0}=$'+'{}m/s'.format(sim.pf['initial_v_stream']), c='r', linestyle='-.')
 plt.title("T_k, T_chi, T_gamma vs. redshift")
-plt.xlabel("z")
-plt.ylabel(r"$T$ (mK)")
+plt.xlabel("1+z")
+plt.ylabel(r"$T$ (K)")
 plt.xlim(10, 1000)
 plt.ylim(1, 2000)
 plt.xscale("log")
 plt.yscale("log")
-plt.legend()
+plt.legend(loc = 'upper left')
 plt.show()
 
 
 
 """ 
-plt.plot(sim.history['z'], sim.history['igm_e'], label='igm_e', linestyle = '-')
-# plt.plot(sim.history['z'], sim.history['cgm_e'], label='cgm_e', linestyle = '-')
+plt.plot(1+sim.history['z'], sim.history['igm_e'], label='igm_e', linestyle = '-')
+# plt.plot(1+sim.history['z'], sim.history['cgm_e'], label='cgm_e', linestyle = '-')
 plt.plot(history_z, history_igm_e, label='igm_e_default', linestyle = '--')
 # plt.plot(history_z, history_cgm_e, label='cgm_e_default', linestyle = '--')
 plt.title("x_e vs. z")
