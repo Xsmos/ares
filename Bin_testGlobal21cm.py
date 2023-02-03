acebece250cd31c7b951537ae54fbc50ae525deb
Xsmos <xiabin.phy@gmail.com>
2023-02-03 13:56:59 -0500

    ploted dTb, but the figure isnt expected.

--- Bin_testGlobal21cm.py
+++ Bin_testGlobal21cm.py
@@ -2,17 +2,23 @@ import ares
 import numpy as np
 import matplotlib.pyplot as plt
 
-sim = ares.simulations.Global21cm(dark_matter_heating=False)
+sim = ares.simulations.Global21cm(radiative_transfer=False, verbose=False, dark_matter_heating=False, include_cgm=False)
 sim.run()
-plt.plot(sim.history['z'], sim.history['dTb'], label='default')
+plt.plot(1+sim.history['z'], sim.history['dTb'],
+             label=r'DM, $V{\chi b,0}=$'+'{}m/s'.format(sim.pf['initial_v_stream']), c='k', linestyle='-')
 
-sim = ares.simulations.Global21cm(dark_matter_heating=True)
-sim.run()
-plt.plot(sim.history['z'], sim.history['dTb'], label='DM, z_initial = 60', linestyle='--')
 
-sim = ares.simulations.Global21cm(dark_matter_heating=True, initial_redshift=300)
+sim = ares.simulations.Global21cm(radiative_transfer=False, verbose=False, dark_matter_heating=True, include_cgm=False, initial_v_stream=0, initial_redshift=1010)
 sim.run()
-plt.plot(sim.history['z'], sim.history['dTb'], label='DM, z_initial = 300')
+plt.plot(1+sim.history['z'], sim.history['dTb'],
+             label=r'DM, $V{\chi b,0}=$'+'{}m/s'.format(sim.pf['initial_v_stream']), c='b', linestyle='--')
+
+
+sim = ares.simulations.Global21cm(radiative_transfer=False, verbose=False, dark_matter_heating=True, include_cgm=False, initial_redshift=1010)
+sim.run()
+plt.plot(1+sim.history['z'], sim.history['dTb'],
+             label=r'DM, $V{\chi b,0}=$'+'{}m/s'.format(sim.pf['initial_v_stream']), c='r', linestyle=':')
+
 
 plt.title("global 21 cm signal vs. redshift")
 plt.xlabel("z")
