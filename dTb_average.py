079e499edb8d24da34c8ed98a275ada112da6c27
Xsmos <xiabin.phy@gmail.com>
2023-02-24 12:29:50 -0500

    created dTb_average.py

---
+++ dTb_average.py
@@ -0,0 +1,19 @@
+import ares
+import matplotlib.pyplot as plt
+import numpy as np
+
+pf = \
+    {
+    'radiative_transfer':False,
+    'verbose':False,
+    'dark_matter_heating':True, 
+    'include_cgm':False, 
+    'initial_v_stream':0, 
+    'initial_redshift':1010, 
+    'include_He':True
+    }
+
+V_rms = 29000 # m/s
+
+def 
+sim = ares.simulations.Global21cm(initial_v_stream = initial_v_stream, **pf)
